3e41b7837ab56e976bf5b0b1dc463bbdca6a25a4
Divakar Shetty <divakarshetty@divakars-mbp.mynet>
2019-08-16 13:31:15 +0100

    Code indented

--- src/service.js
+++ src/service.js
@@ -4,6 +4,7 @@ export function getChatLog() {
   
   return getMessages().then(function(messages) {
     return getMembers().then(function(members) {
+      // Traverse through members array & modify it to match the requirement
       members.map((item) => {
         item.fullName = item.firstName + ' ' + item.lastName;
         item.userId = item.id;
@@ -14,6 +15,7 @@ export function getChatLog() {
         return item;
       })
   
+      // Traverse through messages array & modify it to match the requirement
       messages.map((item) => {
         item.messageId = item.id;
         item.timestamp = new Date(item.timestamp).toDateString();
@@ -21,11 +23,13 @@ export function getChatLog() {
         return item;
       })
 
+      // Sort messages array based on timestamp
       messages.sort(function(a, b) {
         var dateA = new Date(a.timestamp), dateB = new Date(b.timestamp);
         return dateA - dateB;
       });
 
+      // Merge "members" array & "messages" array based on "userId" key to get desired output
       let finalArr = [];
       messages.map(item => {
         members.map(item1 => {
